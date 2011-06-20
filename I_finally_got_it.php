d9187b53c3259176a36f96735e9e809e21f9832b
Your Name <github@yehuda.biz>
2011-06-20 16:03:21 +0000

    commit

---
+++ I_finally_got_it.php
@@ -0,0 +1,48 @@
+<?php
+
+function tag($tag, $attributes, $content){
+
+	if(!is_array($attributes)){
+	
+		die('Not an array');
+	}
+$html = '<' . $tag;
+
+	foreach ($attributes as $key=>$value){	
+	
+	$html .= ' ' . $key . ' =' . $value; 
+	
+	}
+
+$html.=  '>' . $content . '</' . $tag . '>';	
+
+return $html;
+
+}
+
+function arrayDivider($array){
+
+foreach($array as $key=>$value){<?
+
+switch($key){
+case 'title':
+$tag = 'h1';
+$attributes = array();
+
+break;
+
+case 'body':
+$tag = 'p';
+$attributes = array();
+
+}
+echo tag($tag, $attributes, $value);
+}
+
+}
+
+$article = array('title' => 'my article title', 'body' => 'lorem ipsum');
+
+arrayDivider($article);
+
+?>
